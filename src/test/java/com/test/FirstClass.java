186b62fd6c871e2e083e4b8635c20af82351ba24
AlekSound <trigubovich.a@gmail.com>
2019-12-10 21:50:02 +0300

    added driver, dependesies

--- src/test/java/com/test/FirstClass.java
+++ src/test/java/com/test/FirstClass.java
@@ -1,4 +1,29 @@
 package com.test;
 
-public class firstClass {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+
+        WebElement dynamicElement =
+                (new WebDriverWait(driver, 10)).
+                        until(ExpectedConditions.
+                                        presenceOfElementLocated(By.xpath("//*[@title='Faded Short Sleeve T-shirts']")));
+
+        dynamicElement.click();
+
+
+        driver.close();
+
+    }
 }
